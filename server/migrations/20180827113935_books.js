454d1797a438db429dcbd6a32634f4cd92f2a7e6
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-27 12:01:15 -0600

    Added changes to migrations and seed files

--- server/migrations/20180827113935_books.js
+++ server/migrations/20180827113935_books.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('books', books => {
+    books.increments()
+    books.string('Book Title')
+    books.string('Book Genre')
+    books.string('Book Description')
+    books.string('Book Cover Url')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('books')
 };
--- server/migrations/20180827114009_authors.js
+++ server/migrations/20180827114009_authors.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('authors', authors => {
+    authors.increments()
+    authors.string('Author First Name')
+    authors.string('Author Last Name')
+    authors.string('Author Biography')
+    authors.string('Author Portrait Url')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('authors')
 };
