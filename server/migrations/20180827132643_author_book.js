ae5091badba1daedcc4f7f0986f3274493795620
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-27 19:53:30 -0600

    Adjusted Spelling

--- server/migrations/20180827132643_author_book.js
+++ server/migrations/20180827132643_author_book.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('author_book', author_book => {
-   author_book.integer('books_info').references('book.id').onDelete('cascade')
-   author_book.integer('authors_info').references('author.id').onDelete('cascade')
+   author_book.integer('books_info').references('books.id').onDelete('cascade')
+   author_book.integer('authors_info').references('authors.id').onDelete('cascade')
    author_book.primary(['books_info', 'authors_info'])
   })
 };
