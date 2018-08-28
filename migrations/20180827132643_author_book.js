c300162478ae1fdf397830633f08aa9b5b9d8056
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-28 09:59:13 -0600

    Added minor changes

--- migrations/20180827132643_author_book.js
+++ migrations/20180827132643_author_book.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('author_book', author_book => {
-   author_book.integer('books_info').references('books.id').onDelete('cascade')
+   author_book.integer('books_info').references('book.id').onDelete('cascade')
    author_book.integer('authors_info').references('authors.id').onDelete('cascade')
    author_book.primary(['books_info', 'authors_info'])
   })
