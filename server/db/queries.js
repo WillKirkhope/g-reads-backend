14b3a6122a17ffe8e6a13932cb9be980031006ec
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-27 20:13:53 -0600

    Added changes to Queries

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,8 +1,18 @@
 const database = require("./database-connection")
 
 module.exports = {
-    list(tableName) {
-        return database(tableName).select()
+    listBooks(){
+      return database('book')
+        .select('book.cover_url', 'book.title', 'author.first_name', 'author.last_name', 'book.genre', 'book.description')
+        .from('book')
+        .innerJoin('author_book', 'book.id', 'author_book.book_id')
+        .innerJoin('author', 'author.id', 'author_book.author_id')
+    },
+    listAuthors(){
+      return database('author').select('author.pic_url', 'author.first_name', 'author.last_name', 'author.bio', 'book.title')
+        .from('author')
+        .fullOuterJoin('author_book', 'author.id', 'author_book.author_id')
+        .fullOuterJoin('book', 'book.id', 'author_book.book_id')
     },
     read(tableName, id) {
       return database(tableName).select().where('id', id)
